1627523ea99ac3d0d506b3146d6dac616682692f
kazden.jolley <kazden.jolley@gmail.com>
2021-06-01 16:13:22 -0600

    displaying user info and followers

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
 
   state = {
     info: {},
+    followers: []
   }
 
   componentDidMount() {
@@ -20,12 +21,23 @@ class App extends React.Component {
       .catch(err => {
         console.log(err);
       })
+
+    axios
+      .get('https://api.github.com/users/KazdenJolley/followers')
+      .then(res => {
+        this.setState({
+          followers: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   render() {
     return (
       <div className="App">
-        <Card info={this.state.info} />
+        <Card info={this.state.info} followers={this.state.followers} />
       </div>
     );
   }
--- github-user-card/src/components/Card.js
+++ github-user-card/src/components/Card.js
@@ -1,15 +1,22 @@
+import axios from 'axios';
 import React from 'react';
+import Follower from './Follower';
 
 class Card extends React.Component {
-
     render() {
-        const { info } = this.props;
+        const { info, followers } = this.props;
         return (
             <div>
                 <p>Name: {info.name}</p>
                 <p>Bio: {info.bio}</p>
                 <p>Location: {info.location}</p>
-                <p>Followers: {info.followers}</p>
+                <ul>Followers: 
+                    {
+                        followers.map((follower, idx) => {
+                            return <Follower key={idx} follower={follower} />
+                        })
+                    }
+                </ul>
                 <p>Following: {info.following}</p>
                 <a href={info.html_url}>Github Profile</a>
                 <img src={info.avatar_url} alt="logo"/>
